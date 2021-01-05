106858d9ec9ce0fa2e2fc01f60c87d2cf03a561c
christophersiem <christopher.siem@gmail.com>
2021-01-05 17:50:50 +0100

    Add button functionality

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,23 +2,28 @@ package sample;
 
 import javafx.application.Application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
-
-
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
 
     Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Hello World");
         button = new Button();
         button.setText("Click me");
+
+        button.setOnAction(this);
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
@@ -29,7 +34,11 @@ public class Main extends Application {
 
     }
 
-    public static void main(String[] args) {
-        launch(args);
+
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource() == button){
+            System.out.println("Wow, nice click!");
+        }
     }
 }
