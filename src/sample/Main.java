338b48b9fd38c5826e72e1fe2650eebb010ba6db
christophersiem <christopher.siem@gmail.com>
2021-01-05 18:53:11 +0100

    Add exit functionality

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -14,8 +13,6 @@ public class Main extends Application {
     Stage window;
     Scene scene1, scene2;
 
-    Button button;
-
     public static void main(String[] args) {
         launch(args);
     }
@@ -23,6 +20,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         window = primaryStage;
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram();
+        });
 
         Label label1 = new Label("Welcome to my first screen");
         Button button1 = new Button("Go to other screen");
@@ -40,14 +41,26 @@ public class Main extends Application {
         Button button3 = new Button("Click me");
         button3.setOnAction(event -> AlertBox.display("ALERT!!!","Thanks for clicking"));
 
+        Button button4 = new Button("Alternatively Click me");
+        button4.setOnAction(event -> {
+        boolean result = ConfirmBox.display("Choose wisely","Yes or No?");
+        });
+
         //Layout 2
         VBox layout2 = new VBox(20);
-        layout2.getChildren().addAll(label2, button2, button3);
+        layout2.getChildren().addAll(label2, button2, button3, button4);
         scene2 = new Scene(layout2, 300, 300);
 
         window.setScene(scene1);
         window.setTitle("Super Duper GUI");
         window.show();
+    }
+
+    private void closeProgram() {
+        Boolean answer = ConfirmBox.display("Title", "Sure you want to exit?");
+        if(answer){
+            window.close();
+        }
     }
 
 
